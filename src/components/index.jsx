ff082a48fa88f73f0cd7ab9d3e1e44bb6e2b8cb6
aburaihanc <74248037+aburaihanc@users.noreply.github.com>
2020-12-13 08:35:55 +0600

    update

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,36 +3,43 @@ import React, { Component } from "react";
 //styles
 import "./Form.css";
 
-import Image from './Image'
-import Login from './Login'
-import Signup from './Signup'
+import Image from "./Image";
+import Login from "./Login";
+import Signup from "./Signup";
 
 class Form extends Component {
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            currentForm : 'login'
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    changeForm = event => {
-        if (this.state.currentForm === 'login') {
-            this.setState({currentForm: 'signup'})
-        } else {
-            this.setState({currentForm: 'login'})
-        }
+    this.state = {
+      currentForm: "login",
+    };
+  }
+
+  changeForm = () => {
+    if (this.state.currentForm === "login") {
+      this.setState({ currentForm: "signup" });
+    } else {
+      this.setState({ currentForm: "login" });
     }
+  };
 
   render() {
     return (
       <div className="container">
-        <div className="form-wrapper" form={this.changeForm}>
-            <Image />
-            {this.state.currentForm === 'login' ? <Login /> : <Signup />}
-            <span className="send-to">
-            <a href="#" className="login" style={{bottom: '20px'}} onClick={this.changeForm}>
-              {this.state.currentForm === 'login' ? 'Create An Account' : 'Already Have An Account'}
+        <div className="form-wrapper">
+          <Image />
+          {this.state.currentForm === "login" ? <Login /> : <Signup />}
+          <span className="send-to">
+            <a
+              href="#"
+              className="login"
+              style={{ bottom: "20px" }}
+              onClick={this.changeForm}
+            >
+              {this.state.currentForm === "login"
+                ? "Create An Account"
+                : "Already Have An Account"}
               <sapn className="material-icons">arrow_right_alt</sapn>
             </a>
           </span>
